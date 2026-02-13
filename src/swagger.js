cef889927c072621cf1e593fb5773808c30aa5dc
wsousa.dev <72842977+wsousadev@users.noreply.github.com>
2026-02-13 07:53:28 -0300

    Refactor swagger.js to use double quotes

--- src/swagger.js
+++ src/swagger.js
@@ -1,34 +1,33 @@
-import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 
 function setupSwagger(app) {
   const options = {
     definition: {
-      openapi: '3.0.3',
+      openapi: "3.0.3",
       info: {
-        title: 'Swagger EducaBlog',
-        version: '1.0.0',
-        description: '<a>http://localhost:3000/swagger.json/</a>',
+        title: "Swagger EducaBlog",
+        version: "1.0.0",
+        description: "Documentação da API EducaBlog",
       },
-      servers: [
-        { url: 'http://localhost:3000', description: 'Local' }
-      ],
+      servers: [{ url: "/", description: "Servidor atual" }],
       components: {
         securitySchemes: {
-          bearerAuth: { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }
-        }
-      }
+          bearerAuth: { type: "http", scheme: "bearer", bearerFormat: "JWT" },
+        },
+      },
     },
-    apis: [
-      './app.js',
-      './src/**/*.js'
-    ],
+    apis: ["./app.js", "./src/**/*.js"],
   };
 
   const swaggerSpec = swaggerJSDoc(options);
 
-  app.get('/swagger.json', (_req, res) => res.json(swaggerSpec));
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
+  app.get("/swagger.json", (_req, res) => res.json(swaggerSpec));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, { explorer: true }),
+  );
 }
 
 export default setupSwagger;
